649c03d5e103e89629e2ea1786e2f65a2914bfae
Deniz Yolsal <46849613+Yobs2K@users.noreply.github.com>
2022-03-19 19:28:19 +0300

    Changed redirection method in UploadController from String to ModelAndView

--- src/main/java/com/example/coursework13/controller/UploadController.java
+++ src/main/java/com/example/coursework13/controller/UploadController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/upload")
@@ -20,6 +22,7 @@ public class UploadController {
 
 
     private final UploadService uploadService;
+    private final String redirectUrl = "stats";
 
     @Autowired
     public UploadController(UploadService uploadService) {
@@ -32,10 +35,10 @@ public class UploadController {
     }
 
     @PostMapping()
-    public String uploadCsvFile(@RequestParam("transaction") MultipartFile transactions,
-                                @RequestParam("transaction_type") MultipartFile transactionTypes,
-                                @RequestParam("mcc_codes") MultipartFile mccCodes,
-                                @RequestParam("gender_train") MultipartFile genderTrain
+    public ModelAndView uploadCsvFile(@RequestParam("transaction") MultipartFile transactions,
+                                      @RequestParam("transaction_type") MultipartFile transactionTypes,
+                                      @RequestParam("mcc_codes") MultipartFile mccCodes,
+                                      @RequestParam("gender_train") MultipartFile genderTrain
     ) {
         uploadService.deleteAll();
         uploadService.uploadAll(transactions,
@@ -43,8 +46,9 @@ public class UploadController {
                 mccCodes,
                 genderTrain);
 
-        return "redirect:/stats";
+        RedirectView view = new RedirectView();
+        view.setUrl(redirectUrl);
+        view.setExposeModelAttributes(false);
+        return new ModelAndView(view);
     }
-
-
 }
